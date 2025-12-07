d87795197c20bdd2ab53f643de1866edcb66a323
unknown <roomamaha4587@gmail.com>
2025-12-07 21:23:43 +0500

    Describe your changes here

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://MahaMuneer0.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AI-HACKATHON/',
+  baseUrl: 'AI-HACKATHON/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
